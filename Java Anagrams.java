3de46dbaf3ecaf08d51ad5fc8e4bf97cda71bdd8
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-06 11:48:43 +0530

    Update Java Anagrams.java

--- Java Anagrams.java
+++ Java Anagrams.java
@@ -1,23 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
 
-    static boolean isAnagram(String firstWord, String secondWord) {
-     char[] word1 = firstWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
-     char[] word2 = secondWord.toLowerCase().replaceAll("[\\s]", "").toCharArray();
-     Arrays.sort(word1);
-     Arrays.sort(word2);
-     return Arrays.equals(word1, word2);
+    static boolean isAnagram(String a, String b) {
+        / if (a.length() != b.length()) {
+            return false;
+        }
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        int sum = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i=0; i<a.length(); i++) {
+                if (a.charAt(i) == c) {
+                    sum++;
+                }
+                if (b.charAt(i) == c) {
+                    sum--;
+                }
+            }
+            if (sum != 0) {
+                return false;
+            }
+        }
+        return true;
     }
+
     public static void main(String[] args) {
-
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
-        boolean ret=isAnagram(A,B);
-        if(ret)System.out.println("Anagrams");
-        else System.out.println("Not Anagrams");
-
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
     }
 }
